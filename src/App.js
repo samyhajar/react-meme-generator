21d2b44ff4dee3abf45c5652748ed52009b6cc56
Samy Hajar <samy.hajar@gmail.com>
2021-01-27 21:54:01 +0100

    Add project to repo:wq

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Create your own Meme!</p>
+        <p></p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
